d5778e5d139a673245b7c7db1c58bcddf831e59b
anshikasiliconstream-ui <anshika.siliconstream@gmail.com>
2026-04-08 15:48:57 +0530

    enter some output

--- BhcMusic/src/test/java/pages/login.java
+++ BhcMusic/src/test/java/pages/login.java
@@ -28,7 +28,7 @@ public class login {
                 AppiumBy.accessibilityId("Continue With Email")
         )).click();
 
-        System.out.println("Clicked Continue With Email");
+        System.out.println("Clicked Continue With your email address");
 
      // Enter Email
         WebElement emailField = wait.until(
